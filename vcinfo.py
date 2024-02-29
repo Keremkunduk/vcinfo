0b4c93ec005ec23393575c1a348986154a13c262
Keremkunduk <85102172+Keremkunduk@users.noreply.github.com>
2024-02-29 17:28:45 +0300

    Add files via upload

--- vcinfo.py
+++ vcinfo.py
@@ -1,29 +1,33 @@
 import discord
 from discord import option
+import asyncio
+import random
 from datetime import datetime
+from datetime import date
 from tkinter import *
 from time import sleep as sl
 
-token = None
+armut = 926500179006353448
 
-guild_id = None
-yourusername = None
+userid = 680117066740924443
 
 client = discord.Bot()
 
-
+global font 
+font = 'Reddit Sans'
 
 @client.event
 async def on_ready():
     print("Ready\n")
     global guild
-    guild = client.get_guild(guild_id)
+    guild = client.get_guild(910981204625457203)
 
 @client.event
 async def on_voice_state_update(member, before, after):
+    today = str(date.today())
     output = None
     situation = '' #None
-    im_here = False
+    is_keremk_here = False
 
     ch_name = str(after.channel)
     ch = after.channel
@@ -34,6 +38,12 @@ async def on_voice_state_update(member, before, after):
 
     current_time = now.strftime("%H:%M:%S")
 
+#DELETE THIS-----------------------------
+
+    if after.self_stream:
+        situation = ' On Stream'
+    elif before.self_stream:
+        situation = ' Off Stream'
     if after.self_mute:
         situation = ' Muted'
     elif before.self_mute:
@@ -42,20 +52,33 @@ async def on_voice_state_update(member, before, after):
         situation = ' Deafen'
     elif before.self_deaf:
         situation = ' Undeafen'
-    if after.self_stream:
-        situation = ' On Stream'
-    elif before.self_stream:
-        situation = ' Off Stream'
+
+    # if after.self_mute:
+    #     situation = ' Muted'
+    # elif before.self_mute:
+    #     situation = ' Unmuted'
+    # if after.self_deaf:
+    #     situation = ' Deafen'
+    # elif before.self_deaf:
+    #     situation = ' Undeafen'
+    # if after.self_stream:
+    #     situation = ' On Stream'
+    # elif before.self_stream:
+    #     situation = ' Off Stream'
+
+#-----------------------------------------
+
+#IS KEREMK HERE
 
     for i in ch.members:
-        if i.name == yourusername and member.name != yourusername:
-            im_here = True
+        if i.name == 'keremkunduk' and member.name != 'keremkunduk':
+            is_keremk_here = True
     
 
     if not before.channel:
-        output = str(member.name) + f' | Joined {ch_name}{situation}' + ' | ' + current_time
+        output = str(member.name) + f' | Joined {ch_name}{situation}' + ' | ' + current_time + ' | ' + today
 
-        if im_here:
+        if is_keremk_here:
             root = Tk()
             root.geometry("+0+0")
             root.overrideredirect(True)
@@ -63,9 +86,9 @@ async def on_voice_state_update(member, before, after):
             root.wm_attributes("-alpha", 0.01)
             root.resizable(0, 0)
             root.wm_attributes('-alpha', 0.7)
-            timer_display = Label(root, font=('Trebuchet MS', 24, 'bold'), bg='black')
-            timer_display.config(text=f'{member.name} Joined', fg='red')
-            timer_display.pack()
+            display = Label(root, font=(font, 24, 'bold'), bg='black')
+            display.config(text=f'{member.name} Joined', fg='cyan')
+            display.pack()
             root.update()
             sl(2)
             root.destroy()
@@ -73,13 +96,13 @@ async def on_voice_state_update(member, before, after):
 
     if before.channel and after.channel:
         if str(before.channel) != str(ch_name): #if channels are same, then member didn't changed his channel, changed situation
-            output = str(member.name) + f' | {before.channel} -> {ch_name}{situation}' + ' | ' + current_time
+            output = str(member.name) + f' | {before.channel} -> {ch_name}{situation}' + ' | ' + current_time + ' | ' + today
 
             for i in before.channel.members:
-                if i.name == yourusername and member.name != yourusername:
-                    im_here = True
+                if i.name == 'keremkunduk' and member.name != 'keremkunduk':
+                    is_keremk_here = True
 
-            if im_here:
+            if is_keremk_here:
                 root = Tk()
                 root.geometry("+0+0")
                 root.overrideredirect(True)
@@ -87,19 +110,31 @@ async def on_voice_state_update(member, before, after):
                 root.wm_attributes("-alpha", 0.01)
                 root.resizable(0, 0)
                 root.wm_attributes('-alpha', 0.7)
-                timer_display = Label(root, font=('Trebuchet MS', 24, 'bold'), bg='black')
-                # timer_display.config(text=f'{member.name} {before.channel} -> {ch_name}', fg='red')
-                timer_display.config(text=f'{member.name} -> {ch_name}', fg='red')
-                timer_display.pack()
+                display = Label(root, font=(font, 24, 'bold'), bg='black')
+                # display.config(text=f'{member.name} {before.channel} -> {ch_name}', fg='cyan')
+                display.config(text=f'{member.name} -> {ch_name}', fg='cyan')
+                display.pack()
                 root.update()
                 sl(2)
                 root.destroy()
                 root.update()
 
+        # elif after.self_mute:
+            # situation = ' Muted'
+        # elif before.self_mute:
+            # situation = ' Unmuted'
+        # if after.self_deaf:
+            # situation = ' Deafen'
+        # elif before.self_deaf:
+            # situation = ' Undeafen'
+        # if after.self_stream:
+            # situation = ' On Stream'
+        # elif before.self_stream:
+            # situation = ' Off Stream'
     if before.channel and not after.channel:
-        output = str(member.name) + f' | Left {before.channel}{situation}' + ' | ' + current_time #ch_name
+        output = str(member.name) + f' | Left {before.channel}{situation}' + ' | ' + current_time + ' | ' + today #ch_name
 
-        if im_here:
+        if is_keremk_here:
             root = Tk()
             root.geometry("+0+0")
             root.overrideredirect(True)
@@ -107,18 +142,18 @@ async def on_voice_state_update(member, before, after):
             root.wm_attributes("-alpha", 0.01)
             root.resizable(0, 0)
             root.wm_attributes('-alpha', 0.7)
-            timer_display = Label(root, font=('Trebuchet MS', 24, 'bold'), bg='black')
-            timer_display.config(text=f'{member.name} Left', fg='red')
-            timer_display.pack()
+            display = Label(root, font=(font, 24, 'bold'), bg='black')
+            display.config(text=f'{member.name} Left', fg='cyan')
+            display.pack()
             root.update()
             sl(2)
             root.destroy()
             root.update()
 
     if output == None:
-        output = str(member.name) + ' |' + str(situation) + ' | ' + current_time
+        output = str(member.name) + ' |' + str(situation) + ' | ' + current_time + ' | ' + today
         if 'Stream' in situation or 'eafen' in situation:
-            if im_here:
+            if is_keremk_here:
                 root = Tk()
                 root.geometry("+0+0")
                 root.overrideredirect(True)
@@ -126,17 +161,31 @@ async def on_voice_state_update(member, before, after):
                 root.wm_attributes("-alpha", 0.01)
                 root.resizable(0, 0)
                 root.wm_attributes('-alpha', 0.7)
-                timer_display = Label(root, font=('Trebuchet MS', 24, 'bold'), bg='black')
-                timer_display.config(text=f'{member.name}{situation}', fg='red')
-                timer_display.pack()
+                display = Label(root, font=(font, 24, 'bold'), bg='black')
+                display.config(text=f'{member.name}{situation}', fg='cyan')
+                display.pack()
                 root.update()
                 sl(2)
                 root.destroy()
                 root.update()
     
     print(output)
-    op = open('vclog.txt', 'a')
+    op = open('vclog.txt', 'a', encoding='utf-8')
     op.write(output + '\n')
     op.close
+    
+    # if not before.channel and after.channel:
+    #     kanalid = 1063747294215815230
 
-client.run(token)
+    #     channel = client.get_channel(kanalid)
+    #     members = channel.members
+    #     if members != []:
+    #         for x in members:
+    #             if x.id == member.id:
+    #                 print(f'{member} Bağlandı')
+    #             else:
+    #                 print(f'{member} Ayrıldı')
+    #     else:
+    #         print(f'{member} Ayrıldı')               
+
+client.run('OTI2NTAwMTc5MDA2MzUzNDQ4.GHjtxl.-bP8dpb0hz8hAfc8QnrgEEZR9bgmINV208SQbs')
